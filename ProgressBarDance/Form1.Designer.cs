261664b2c9f2f7f4f14fbdde5da5c616419b9646
Tanyababinets <tanya060616@gmail.com>
2023-09-04 15:25:14 +0300

    Add project files.

---
+++ ProgressBarDance/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace ProgressBarDance
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.progressBar4 = new System.Windows.Forms.ProgressBar();
+            this.progressBar5 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(120, 35);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(325, 44);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(120, 100);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(325, 44);
+            this.progressBar2.TabIndex = 1;
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(120, 169);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(325, 44);
+            this.progressBar3.TabIndex = 2;
+            // 
+            // progressBar4
+            // 
+            this.progressBar4.Location = new System.Drawing.Point(120, 233);
+            this.progressBar4.Name = "progressBar4";
+            this.progressBar4.Size = new System.Drawing.Size(325, 44);
+            this.progressBar4.TabIndex = 3;
+            // 
+            // progressBar5
+            // 
+            this.progressBar5.Location = new System.Drawing.Point(120, 296);
+            this.progressBar5.Name = "progressBar5";
+            this.progressBar5.Size = new System.Drawing.Size(325, 44);
+            this.progressBar5.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(209, 369);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 57);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar5);
+            this.Controls.Add(this.progressBar4);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar5;
+        private System.Windows.Forms.ProgressBar progressBar4;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.ProgressBar progressBar2;
+    }
+}
+
---
+++ ProgressBarDance/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using ProgressBar = System.Windows.Forms.ProgressBar;
+
+namespace ProgressBarDance
+{
+    public partial class Form1 : Form
+    {
+        public SynchronizationContext uiContext;
+
+        List<ProgressBar> pb = null;
+
+        Random rand = new Random();
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            uiContext = SynchronizationContext.Current;
+            pb = new List<ProgressBar> { progressBar1, progressBar2, progressBar3, progressBar4, progressBar5 };
+           
+
+        }
+
+        public void ThreadFunk(object obj)
+        {
+
+            ProgressBar pb2 = (ProgressBar)obj;
+
+
+            uiContext.Send(d => pb2.Minimum = 0, null);
+            uiContext.Send(d => pb2.Maximum = 230, null);
+            uiContext.Send(d => pb2.Value = 0, null);          
+            int count = 0;
+            while (count != 200)
+            {
+                uiContext.Send(d => pb2.Value = rand.Next(1, 230), null);
+                Thread.Sleep(100);
+                count++;             
+            }
+        }
+        private void button1_Click(object sender, EventArgs e) 
+        {
+            button1.Enabled = false;
+
+            foreach (var t in pb)// Создание и запуск потоков для каждого ProgressBar
+            {
+                Thread thread = new Thread(new ParameterizedThreadStart(ThreadFunk));
+                thread.IsBackground = true;
+                thread.Start(t);
+
+                Thread.Sleep(100);
+            }            
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+          
+        }
+
+    }
+}
+
+
